71d6e574a3f209cb30a427ccaf86956779211b06
marcelos91 <marcelo.severo@hotmail.com>
2023-02-09 15:27:41 -0300

    layout

---
+++ app/src/main/java/com/example/layout/FragmentTela1.kt
@@ -0,0 +1,7 @@
+package com.example.layout
+
+import androidx.fragment.app.Fragment
+
+class FragmentTela1 : Fragment(R.layout.fragment_tela1) {
+
+}
---
+++ app/src/main/java/com/example/layout/FragmentTela2.kt
@@ -0,0 +1,11 @@
+package com.example.layout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentTela2 : Fragment(R.layout.fragment_tela2) {
+
+}
